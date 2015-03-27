b967035b4ad500b5e38aadee2702fb528520bb1f
Vangie Shue <vangieshue@gmail.com>
2015-03-27 18:35:41 -0400

    Create NYUPT20150327_F.java

---
+++ NYUPT20150327_F.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int moments = scan.nextInt();
+    int skip = scan.nextInt();
+    int watched = 0;
+    int curr = 1;
+    for(int m=0;m<moments;m++) {
+      int = scan.nextInt();
+      int  = scan.nextInt();
+      while(curr+skip<pos) {
+        curr=curr+skip;
+      }
+      watched = watched+pos-curr;
+      watched = watched+duration;
+      
+    }
+    
+  }
+}
