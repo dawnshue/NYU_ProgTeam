52975c17f516e91361067eb8bc41997f248d7600
Vangie Shue <vangieshue@gmail.com>
2015-04-03 18:54:56 -0400

    Update NYUPT20150403_B.java

--- NYUPT20150403_B.java
+++ NYUPT20150403_B.java
@@ -18,9 +18,22 @@ public class Main {
   private static String evaluate(int n, int m, char[][] matrix) {
     for(int i=0; i<n-1; i++) {
       for(int i2=0; i2<m-1; i2++) {
-        if(hasCycle(matrix,i,i2)) return "Yes";
+        if(hasCycle(matrix,i,i2,i,i2,i,i2)) return "Yes";
       }
     }
     return "No";
+  }
+  private static int hasCycle(char[][] matrix, x, y, px, py, x2, y2) {
+    if((px!=x2 || py!=y2) && x==x2 && y==y2) {
+      return true;
+    }
+    if(x2>0 && matrix[x2-1][y2]==matrix[x2][y2] && !((x2-1)!=px && y2!=py)) {
+      return hasCycle(matrix, x, y, x2, y2, x2-1, y2);
+    }
+    if(y2>0 && matrix[x2][y2-1]==matrix[x2][y2] && !(x2!=px && (y2-1)!=py)) {
+      return hasCycle(matrix, x, y, x2, y2, x2, y2-1);
+    }
+    
+    return false;
   }
 }
