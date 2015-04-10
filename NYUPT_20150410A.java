1eb6bd979d750e6b17fe30eb42dd3ec28398f225
Vangie Shue <vangieshue@gmail.com>
2015-04-10 17:29:09 -0400

    Update NYUPT_20150410A.java

--- NYUPT_20150410A.java
+++ NYUPT_20150410A.java
@@ -1,4 +1,32 @@
 import java.util.*;
 public Main {
-
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    int len = s.nextInt();
+    int[] heights = new int[len];
+    for(int i=0; i<len; i++) {
+      heights[i] = s.nextInt();
+    }
+    System.out.println(bestpair(heights, len));
+  }
+  private static String bestpainr(int[] heights, int len) {
+    int p1 = 1;
+    int p2 = 2;
+    int min = Math.abs(heights[1]-heights[0]);
+    
+    int np1 = 1;
+    int np2 = 2;
+    while(np2<len) {
+      if(Math.abs(heights[np2++]-heights[np1++])<min) {
+        p1=np1;
+        p2=np2;
+      }
+    }
+    if(Math.abs(heights[0]-heights[len-1])<min) {
+      p1=1;
+      p2=len;
+    }
+    
+    return new String(p1+" "+p2);
+  }
 }
