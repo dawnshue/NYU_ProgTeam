1601d898ff92d98dedf180e52881d4d9752e1779
Vangie Shue <vangieshue@gmail.com>
2015-04-10 18:10:10 -0400

    Update NYUPT20150410_B.java

--- NYUPT20150410_B.java
+++ NYUPT20150410_B.java
@@ -1,4 +1,21 @@
 import java.util.*;
 public class Main {
+  HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+  Queue<Integer> q = new LinkedList<Integer>();
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    while(s.hasNextInt()) {
+      int key = s.nextInt();
+      if(map.containsKey(key)) {
+        map.put(key,map.get(key)+1);
+      } else {
+        map.put(key,1);
+        q.add(key);
+      }
+    }
+    for(Integer i : q) {
+      System.out.println(i+" "+map.get(i));
+    }
+  }
 
 }
