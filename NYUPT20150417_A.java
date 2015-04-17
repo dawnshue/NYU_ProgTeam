08d39ddd63ff6288fefec4ea2bece48dc6272ee3
Vangie Shue <vangieshue@gmail.com>
2015-04-17 17:36:47 -0400

    Update NYUPT20150417_A.java

--- NYUPT20150417_A.java
+++ NYUPT20150417_A.java
@@ -8,20 +8,19 @@ public class Main {
     System.out.println(english(n));
   }
   
-  private static String english(int n) {
+   private static String english(int n) {
     String[] singles = {"zero","one","two","three","four","five","six","seven","eight","nine"};
-    String[] uniq = {"ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
+    String[] uniq = {"ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen",
+    "seventeen","eighteen","nineteen"};
     String[] tens = {"","ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
-    String ans;
-    if(n>=10 || n<=19) {
-      ans = uniq[n-10];
+    if(n>=10 && n<=19) {
+      return uniq[n-10];
     } else if(n<=9) {
-      ans = singles[n];
+      return singles[n];
     } else if(n%10==0) {
-      ans = tens[n/10];
+      return tens[n/10];
     } else {
-      ans = tens[(n-n%10)/10]+singles[n%10];
+      return tens[(n-n%10)/10]+singles[n%10];
     }
-    return ans;
   }
 }
