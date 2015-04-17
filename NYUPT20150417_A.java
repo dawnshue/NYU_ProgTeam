c4ad654c89b68f33704aff51e512c6d83ffdf521
Vangie Shue <vangieshue@gmail.com>
2015-04-17 17:40:33 -0400

    Update NYUPT20150417_A.java

--- NYUPT20150417_A.java
+++ NYUPT20150417_A.java
@@ -2,10 +2,10 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     
-    //Scanner scan = new Scanner(System.in);
-    //int n = scan.nextInt();
-    for(int n=0; n<=99; n++)
-    System.out.println(english(n));
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    //for(int n=0; n<=99; n++)
+    //System.out.println(english(n));
   }
   
    private static String english(int n) {
@@ -20,7 +20,7 @@ public class Main {
     } else if(n%10==0) {
       return tens[n/10];
     } else {
-      return tens[(n-n%10)/10]+singles[n%10];
+      return tens[(n-n%10)/10]+"-"+singles[n%10];
     }
   }
 }
