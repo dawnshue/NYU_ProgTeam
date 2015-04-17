70b7eca900e9cc0317a6b41d135476723d76a424
Vangie Shue <vangieshue@gmail.com>
2015-04-17 17:51:57 -0400

    Update NYUPT20150417_B.java

--- NYUPT20150417_B.java
+++ NYUPT20150417_B.java
@@ -1,4 +1,19 @@
 import java.util.*;
 public class Main {
-
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int input = scanner.nextInt();
+    System.out.println(rank(input));
+  }
+  
+  private static int rank(int input) {
+    int places = 0;
+    while((input-input%(Math.pow(10,places)))/(Math.pow(10,places)) >= 10) {
+      places++;
+    }
+    for(int i = 0; i<places; i++) {
+      
+    }
+    
+  }
 }
