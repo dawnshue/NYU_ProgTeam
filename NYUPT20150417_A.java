54bc33fb5b270a8b7c914852199e1703aa21b624
Vangie Shue <vangieshue@gmail.com>
2015-04-17 17:31:13 -0400

    Update NYUPT20150417_A.java

--- NYUPT20150417_A.java
+++ NYUPT20150417_A.java
@@ -1,12 +1,17 @@
 import java.util.*;
 public class Main {
   public static void main(String[] args) {
+    
+    //Scanner scan = new Scanner(System.in);
+    //int n = scan.nextInt();
+    for(int n=0; n<=99; n++)
+    System.out.println(english(n));
+  }
+  
+  private static String english(int n) {
     String[] singles = {"zero","one","two","three","four","five","six","seven","eight","nine"};
     String[] uniq = {"ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
     String[] tens = {"","ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
-    
-    Scanner scan = new Scanner(System.in);
-    int n = scan.nextInt();
     String ans;
     if(n>=10 || n<=19) {
       ans = uniq[n-10];
@@ -17,11 +22,6 @@ public class Main {
     } else {
       ans = tens[(n-n%10)/10]+singles[n%10];
     }
-    System.out.println(english(n));
-  }
-  
-  private static String english(int n) {
-    
     return ans;
   }
 }
