3bb73c5ce197c2384091cf3eefadd9b3d65ff89c
Vangie Shue <vangieshue@gmail.com>
2015-04-24 17:40:56 -0400

    Update NYUPT20150424_A.java

--- NYUPT20150424_A.java
+++ NYUPT20150424_A.java
@@ -1,11 +1,13 @@
 import java.util.*;
 public class Main {
+  private String ans;
   public static void main(String[] args) {
     //add all numbers to queue
     //pop top of queue and check end of current list
     Scanner s = new Scanner(System.in);
     int students = s.nextInt();
-    System.out.println(longest(students));
+    System.out.println(this.longest(students));
+    System.out.println(ans);
   }
   private int longest(int n) {
     Queue<Integer> q = new LinkedList<Integer>();
@@ -14,9 +16,29 @@ public class Main {
     int last = 1
     StringBuilder s = new StringBuilder("1");
     for(int i=2; i<=n; i++) {
+      if((i-last)==1) {
+        q.add(i)
+      } else {
+        s.append(" "+i);
+        last = i;
+        count++;
+      }
       if(q.size()>0) {
-        
+        while(q.size()>0) {
+          int temp = q.poll();
+          if(Math.abs((temp-last))==1) {
+            q2.add(temp);
+          } else {
+            s.append(" "+temp);
+            last = temp;
+            count++;
+          }
+        }
+        while(q2.size()>0) {
+          q.add(q2.poll());
+        }
       }
     }
+    return count;
   }
 }
