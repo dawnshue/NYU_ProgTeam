4a4edd2763520c4544e62aaeb90fa52d55b9f726
Vangie Shue <vangieshue@gmail.com>
2015-04-24 18:13:36 -0400

    Update NYUPT20150424_B.java

--- NYUPT20150424_B.java
+++ NYUPT20150424_B.java
@@ -3,19 +3,24 @@ public class Main {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     int tests = scan.nextInt();
+    List<Integer> l1;
+    List<Integer> l2;
     for(int test=0; test<tests; test++) {
-      int[] l1 = new int[scan.nextInt()];
-      int[] l2 = new int[scan.nextIn()];
-      for(int p=0; p<l1.length; p++) {
-        l1[p] = scan.nextInt();
+      l1 = new ArrayList<Integer>();
+      int l1l = scan.nextInt();
+      l2 = new ArrayList<Integer>();
+      int l2l = scan.nextIn();
+      for(int p=0; p<l1l; p++) {
+        l1.add(scan.nextInt());
       }
-      for(int p=0; p<l2.length; p++) {
-        l2[p] = scan.nextInt();
+      for(int p=0; p<l2l; p++) {
+        l2.add(scan.nextInt());
       }
       System.out.println(getLeast(l1, l2);
     }
   }
-  private static int getLeast(int[] l1, int[] l2) {
-    
+  private static int getLeast(List<Integer> l1, List<Integer> l2) {
+    Collections.sort(l1);
+    Collections.sort(l2);
   }
 }
