d6bc4bda35068ac02d11703c5bd82c34419563bf
Vangie Shue <vangieshue@gmail.com>
2015-04-24 18:08:35 -0400

    Update NYUPT20150424_B.java

--- NYUPT20150424_B.java
+++ NYUPT20150424_B.java
@@ -1,4 +1,21 @@
 import java.util.*;
 public class Main {
-
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int tests = scan.nextInt();
+    for(int test=0; test<tests; test++) {
+      int[] l1 = new int[scan.nextInt()];
+      int[] l2 = new int[scan.nextIn()];
+      for(int p=0; p<l1.length; p++) {
+        l1[p] = scan.nextInt();
+      }
+      for(int p=0; p<l2.length; p++) {
+        l2[p] = scan.nextInt();
+      }
+      System.out.println(getLeast(l1, l2);
+    }
+  }
+  private static int getLeast(int[] l1, int[] l2) {
+    
+  }
 }
