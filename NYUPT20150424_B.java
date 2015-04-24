72f994cf9c2e319c4c872c1934d1b7db11246656
Vangie Shue <vangieshue@gmail.com>
2015-04-24 18:51:42 -0400

    Update NYUPT20150424_B.java

--- NYUPT20150424_B.java
+++ NYUPT20150424_B.java
@@ -9,14 +9,14 @@ public class Main {
       l1 = new ArrayList<Integer>();
       int l1l = scan.nextInt();
       l2 = new ArrayList<Integer>();
-      int l2l = scan.nextIn();
+      int l2l = scan.nextInt();
       for(int p=0; p<l1l; p++) {
         l1.add(scan.nextInt());
       }
       for(int p=0; p<l2l; p++) {
         l2.add(scan.nextInt());
       }
-      System.out.println(getLeast(l1, l2);
+     System.out.println(getLeast(l1, l2));
     }
   }
   private static int getLeast(List<Integer> l1, List<Integer> l2) {
@@ -26,19 +26,29 @@ public class Main {
     int i=0;
     int i2=0;
     while(i<l1.size()) {
+System.out.println(i);
       int temp = l1.get(i);
-      while(l2.get(i2)!=temp && l2.get(i2)<temp && i2<l2.size()) {
+      while(l2.get(i2)<temp && i2<l2.size()) {
         i2++;
         count++;
       }
-      while(l1.get(i)==l2.get(i2) && i<l1.size() && i2<l2.size()) {
+      while( i<l1.size() && i2<l2.size() && l1.get(i)==l2.get(i2) && l1.get(i)==temp) {
         i++;
         i2++;
+      }
+      while(l1.get(i)==temp && i<l1.size()) {
+          i++;
+          count++;
+      }
+      while(l2.get(i2)==temp && i2<l2.size()) {
+          i2++;
+          count++;
       }
     }
     while(i2<l2.size()) {
       i2++;
       count++;
     }
+    return count;
   }
 }
