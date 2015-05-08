afa9ca1f3e5650ee499ba35465ada008e9611dc8
Vangie Shue <vangieshue@gmail.com>
2015-05-08 18:03:37 -0400

    Update NYUPT20150508_A.java

--- NYUPT20150508_A.java
+++ NYUPT20150508_A.java
@@ -4,7 +4,45 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int input = scanner.nextInt();
         
-        int half = (int)Math.sqrt(input);
-        
+        System.out.println(getSemiPrimes(input));
+    }
+    private static int getSemiPrimes(int input) {
+        List<Integer> list = new LinkedList<Integer>();
+        if(input>2) {
+            list.add(2);
+        }
+        if(input>3) {
+            list.add(3);
+        }
+        for(int i=5; i<=input/2; i=i+2) {
+            if(isPrime(i)) {
+                list.add(i);
+            }
+        }
+        /*
+        for(Integer t : list) {
+            System.out.print(t+" ");
+        }
+        System.out.println();
+        */
+        int count=0;
+        for(int x=0; x<list.size()-1; x++) {
+            for(int y=x+1; y<list.size(); y++) {
+                if(list.get(x)*list.get(y)<=input) {
+                    count++;
+                } else {
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+    private static boolean isPrime(int input) {
+        for(int i=3; i<input; i++) {
+            if(input%i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
