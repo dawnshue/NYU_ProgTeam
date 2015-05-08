18316b1471701f82b5fa47921de13fcb7c78798d
Vangie Shue <vangieshue@gmail.com>
2015-05-08 17:27:46 -0400

    Update and rename NYUPT_20150508.java to NYUPT20150508_A.java

---
+++ NYUPT20150508_A.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        
+        int half = (int)Math.sqrt(input);
+        
+    }
+}
